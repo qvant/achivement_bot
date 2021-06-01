95e7ff25d60a12ae29d64596b35c8b6894418fdb
Andrew Kuskov <qvant86@gmail.com>
2021-06-01 21:11:03 +0300

    don't save excessive translations

--- lib/achievement.py
+++ lib/achievement.py
@@ -24,6 +24,8 @@ class Achievement:
         self.game_id = game_id
 
     def save(self, cursor, active_locale: str):
+        en_name = ""
+        en_descr = ""
         if self.id is None:
             if active_locale == 'en':
                 cursor.execute(
@@ -32,7 +34,7 @@ class Achievement:
                             on conflict ON CONSTRAINT u_achievements_ext_key do update
                             set dt_update=current_timestamp, name=EXCLUDED.name, description=EXCLUDED.description
                             where l.name != EXCLUDED.name
-                            returning id
+                            returning id, name, description
                     """, (self.name, self.ext_id, self.platform_id, self.game_id, self.description)
                 )
             else:
@@ -42,28 +44,33 @@ class Achievement:
                             on conflict ON CONSTRAINT u_achievements_ext_key do update
                             set dt_update=current_timestamp, name=EXCLUDED.name, description=EXCLUDED.description
                             where l.name != EXCLUDED.name
-                            returning id
+                            returning id, name, description
                     """, (self.name, self.ext_id, self.platform_id, self.game_id, self.description)
                 )
             ret = cursor.fetchone()
             if ret is not None:
                 self.id = ret[0]
-            else:
-                cursor.execute("""
-                                    select id from achievements_hunt.achievements where platform_id = %s and ext_id = %s
-                                    and game_id = %s
-                                """, (self.platform_id, self.ext_id, self.game_id))
-                ret = cursor.fetchone()
-                if ret is not None:
-                    self.id = ret[0]
-        cursor.execute("""insert into achievements_hunt.achievement_translations as l
-                        (platform_id, game_id, achievement_id, locale, name, description )
-                        values(%s, %s, %s, %s, %s, %s)
-                        on conflict ON CONSTRAINT u_achievement_translations_key do update
-                        set dt_update=current_timestamp, name=EXCLUDED.name, description=EXCLUDED.description
-                        where l.name != EXCLUDED.name
-                        returning id
-                """, (self.platform_id, self.game_id, self.id, active_locale, self.name, self.description))
+                en_name = ret[1]
+                en_descr = ret[2]
+        if self.id is None:
+            cursor.execute("""
+                                select id, name, description from achievements_hunt.achievements where platform_id = %s and ext_id = %s
+                                and game_id = %s
+                            """, (self.platform_id, self.ext_id, self.game_id))
+            ret = cursor.fetchone()
+            if ret is not None:
+                self.id = ret[0]
+                en_name = ret[1]
+                en_descr = ret[2]
+        if (en_name != self.name or en_descr != self.description) and active_locale != 'en':
+            cursor.execute("""insert into achievements_hunt.achievement_translations as l
+                            (platform_id, game_id, achievement_id, locale, name, description )
+                            values(%s, %s, %s, %s, %s, %s)
+                            on conflict ON CONSTRAINT u_achievement_translations_key do update
+                            set dt_update=current_timestamp, name=EXCLUDED.name, description=EXCLUDED.description
+                            where l.name != EXCLUDED.name
+                            returning id
+                    """, (self.platform_id, self.game_id, self.id, active_locale, self.name, self.description))
 
     def __str__(self):
         return "id: {0}, game_id: {1}, ext_id: {2}".format(self.id, self.game_id, self.ext_id)
