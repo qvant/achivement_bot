cdb022e7247cee0eb913b893b3eb7af183e2b0f2
Andrew Kuskov <qvant86@gmail.com>
2021-07-07 00:30:19 +0300

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -2,11 +2,11 @@
 export APP_HOME="/usr/app/achievement_bot"
 cd $APP_HOME
 sleep 45
-python3 $APP_HOME/main.py --mode=core & disown
+python3 $APP_HOME/main.py --mode=bot & disown
 sleep 5
 python3 $APP_HOME/main.py --mode=updater & disown
 sleep 5
 python3 $APP_HOME/main.py --mode=worker & disown
 sleep 5
-python3 $APP_HOME/main.py --mode=bot & disown
+python3 $APP_HOME/main.py --mode=core & disown
 
