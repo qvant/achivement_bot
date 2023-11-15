4bef28271f07ea705475dd05e1881baf2062ab43
Andrew Kuskov <qvant86@gmail.com>
2023-11-15 08:59:53 +0300

    syntax fix

--- db/migrations/migr_0008.sql
+++ db/migrations/migr_0008.sql
@@ -1,4 +1,4 @@
-create index idx_update_history_active on achievements_hunt.update_history(id_platform, id_process, ) where dt_ended is null;
+create index idx_update_history_active on achievements_hunt.update_history(id_platform, id_process) where dt_ended is null;
 
 drop index u_players_telegram;
 create unique index u_players_telegram on achievements_hunt.players(telegram_id, platform_id);
