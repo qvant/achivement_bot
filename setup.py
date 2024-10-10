09a2edbd734fe7b7346f3e042ed43c372ab49265
qvant <qvant86@gmail.com>
2024-10-11 00:10:39 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="achievement_bot",  # Name
+    version="1.18.0",
+    packages=find_packages()  # Automatically find the packages that are recognized in the '__init__.py'.
+)
